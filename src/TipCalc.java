e5e84a50ce4b3e0bac4437b35c8a43fc4c17dc50
BT_4N2_14 <hsun7920@bths.edu>
2022-09-21 12:09:54 -0400

    added the system print, rounded to the second decimal place.

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class TipCalc {
     public static void main(String[] args) {
-        System.out.print("please input your bill: ");
+        System.out.print("please input your bill: $");
         Scanner calculator = new Scanner(System.in);
         double bill = calculator.nextDouble();
 
-        System.out.print("please input your desired tip percentage: ");
+        System.out.print("please input your desired tip percent: ");
         double tipPercent = calculator.nextDouble();
+
+        System.out.print("please input number of people in your party: ");
+        int numOfPpl = calculator.nextInt();
+
+        double tipPerPerson = Math.round(((tipPercent/100.00) * bill) * 100.00) / 100.00;
+
+
+        double totalPerPerson = Math.round((tipPerPerson + bill / numOfPpl) * 100.00) / 100.00;
+
+        System.out.println("Tip per person: $" + tipPercent);
+        System.out.println("Total per person: $" + totalPerPerson);
     }
 }
